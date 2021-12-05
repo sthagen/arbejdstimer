e7279f45f7ff06f199a8741e3617c0508bca45fd
Stefan Hagen <stefan@hagen.link>
2021-12-05 19:26:53 +0100

    Created the arbejdstimer main

--- arbejdstimer/__main__.py
+++ arbejdstimer/__main__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long,missing-module-docstring
+import sys
 
+from arbejdstimer.cli import app
+
+if __name__ == '__main__':
+    sys.exit(app(prog_name='arbejdstimer'))  # pragma: no cover
