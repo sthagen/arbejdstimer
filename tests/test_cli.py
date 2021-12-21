3491f4071fc2eb6f19295f8bcd72b0d1f1718f89
Stefan Hagen <stefan@hagen.link>
2021-12-21 11:33:15 +0100

    Activated (and corrected) parts of test cases sleeping at the tail of exception blocks

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,29 +18,29 @@ def test_main_legacy_ok(capsys):
 
 def test_version_ok(capsys):
     with pytest.raises(click.exceptions.Exit) as exec_info:
-        cli.app_version() == 0
-        assert exec_info.value.code == 0
-        out, err = capsys.readouterr()
-        assert 'version' in out.lower()
-        assert not err
+        assert cli.app_version() == 0
+    assert exec_info.value.exit_code == 0
+    out, err = capsys.readouterr()
+    assert 'version' in out.lower()
+    assert not err
 
 
 def test_now_ok(capsys):
     in_path = pathlib.Path('tests', 'fixtures', 'basic', 'minimal-config.json')
     with pytest.raises(SystemExit) as exec_info:
-        cli.now(conf=in_path)
-        assert exec_info.value.code == 0
-        out, err = capsys.readouterr()
-        assert 'would extract html tables' in out.lower()
-        assert not err
+        cli.now(conf=in_path)  # type: ignore
+    assert exec_info.value.code == 0
+    out, err = capsys.readouterr()
+    assert 'read valid configuration: ({})' in out.lower()
+    assert not err
 
 
 def test_translate_non_existing_html(capsys):
     in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.json')
     with pytest.raises(SystemExit) as exec_info:
-        cli.now(conf=in_path)
-        assert exec_info.value.code == 1
-        out, err = capsys.readouterr()
-        assert 'source' in out.lower()
-        assert 'is no file' in out.lower()
-        assert not err
+        cli.now(conf=in_path)  # type: ignore
+    assert exec_info.value.code == 1
+    out, err = capsys.readouterr()
+    assert 'config' in err.lower()
+    assert 'is no file' in err.lower()
+    assert not out
