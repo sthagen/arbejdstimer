5e30616f91845e37aa5ac1544827463f0ad0f878
Stefan Hagen <stefan@hagen.link>
2021-12-21 13:39:59 +0100

    Refactored implementation tests to simplify and use central fixtures

--- tests/test_arbejdstimer.py
+++ tests/test_arbejdstimer.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
-import pathlib
-
 import arbejdstimer.arbejdstimer as at
+import tests.conftest as fix
 
 
 def test_at_main():
-    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'minimal-config.json'))
-    assert at.main(['now', inp]) == 0
+    assert at.main(['now', str(fix.CFG_FS_EMPTY)]) == 0
 
 
 def test_at_verify_request_too_few():
-    assert at.verify_request([1]) == (2, 'received wrong number of arguments', [''])
+    assert at.verify_request([1]) == (2, 'received wrong number of arguments', [''])  # type: ignore
 
 
 def test_at_verify_request_unknown_command():
@@ -19,13 +17,11 @@ def test_at_verify_request_unknown_command():
 
 
 def test_at_verify_request_falsy_input():
-    argv = ['now', '']
-    assert at.verify_request(argv) == (2, 'configuration missing', [''])
+    assert at.verify_request(['now', '']) == (2, 'configuration missing', [''])
 
 
 def test_at_main_holidays(capsys):
-    inp = str(pathlib.Path('tests', 'fixtures', 'basic', 'holidays-config.json'))
-    assert at.main(['now', inp]) == 0
+    assert at.main(['now', str(fix.CFG_FS_HOLIDAYS)]) == 0
     out, err = capsys.readouterr()
     assert "'label': 'company holidays 2021/2022'" in out.lower()
     assert not err
