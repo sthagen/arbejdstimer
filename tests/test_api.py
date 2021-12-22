b07f6f7b6cc75c155457961cca56012c23eb3931
Stefan Hagen <stefan@hagen.link>
2021-12-22 17:20:09 +0100

    Added another negative test case for the Hour type of the model

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,3 +20,9 @@ def test_api_hour_42():
     with pytest.raises(ValidationError, match=_subs(1, 'Hour')) as err:
         _ = api.Hour(__root__='42')  # type: ignore
     assert '\n__root__\n  ensure this value is less than or equal to 23' in str(err.value)
+
+
+def test_api_hour_negative():
+    with pytest.raises(ValidationError, match=_subs(1, 'Hour')) as err:
+        _ = api.Hour(__root__='-1')  # type: ignore
+    assert '\n__root__\n  ensure this value is greater than or equal to 0' in str(err.value)
