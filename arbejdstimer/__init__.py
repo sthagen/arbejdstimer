d85f93a41d3d990e1745bab48711d072aca78278
Stefan Hagen <stefan@hagen.link>
2022-10-03 20:25:01 +0200

    Minimal version buildable init

--- arbejdstimer/__init__.py
+++ arbejdstimer/__init__.py
@@ -1,2 +1,7 @@
-"""Working hours (Danish arbejdstimer) or not?"""
-__version__ = '2022.7.24'
+# [[[fill git_describe()]]]
+__version__ = '2022.10.2+parent.03bda739'
+# [[[end]]] (checksum: 56721c6ad953105cc6c64a09dc952a5a)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: list[str] = []
