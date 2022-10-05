e74d190cbb50320e408403f3741e860af40d788e
Stefan Hagen <stefan@hagen.link>
2022-10-05 23:05:44 +0200

    Bumped impl version again for release

--- arbejdstimer/__init__.py
+++ arbejdstimer/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.10.5+parent.930d1a72'
-# [[[end]]] (checksum: 4e1fa2fb7844ab7f0a3ffe11740134f9)
+__version__ = '2022.10.5+parent.8cca9741'
+# [[[end]]] (checksum: ca4e367ea359c14e908b987b3136e11c)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
