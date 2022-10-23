68327e8176f2b41c167a2c48461c9c4fe4fd5f16
Stefan Hagen <stefan@hagen.link>
2022-10-23 14:20:21 +0200

    Bumped impl version for release

--- arbejdstimer/__init__.py
+++ arbejdstimer/__init__.py
@@ -2,8 +2,8 @@ import calendar
 import datetime as dti
 
 # [[[fill git_describe()]]]
-__version__ = '2022.10.22+parent.ae9945dd'
-# [[[end]]] (checksum: 3018e9f747620ed2b1b20cae4c524bcc)
+__version__ = '2022.10.23+parent.712c72f1'
+# [[[end]]] (checksum: fb8505be2ec35cc533109c55fca3871f)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
